4e0d1388bde49fa2814286838e1038d1557b1a68
zeezu124 <zeezu124@gmail.com>
2021-10-25 11:57:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const eh = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React {2 -10}</h1>
+        <h2>Hell {eh ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
